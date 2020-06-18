f1ee2de0f2a2d741f029a509ce2ae12079db839e
Tom W Hartung <tomwhartung@gmail.com>
2020-06-18 10:32:39 -0600

    Updated to catch urls for unknown pages and serve up a 404 page when that happens.

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -1,9 +1,10 @@
 
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
+    re_path(r'^(?P<unknown_page>.*)$', views.not_found, name='not_found'),
 ]
