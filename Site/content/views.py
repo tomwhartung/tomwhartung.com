7ba2035fa6424a5d622810e675c39d8d66bc0d18
Tom W Hartung <tomwhartung@gmail.com>
2020-06-18 10:33:55 -0600

    Added a not_found view that renders the 404 template.

--- Site/content/views.py
+++ Site/content/views.py
@@ -3,9 +3,23 @@ from django.shortcuts import render
 
 
 def home(request):
+
+    """ View for the Home page """
+
     title = 'Home - TomWHartung.com';
     template = 'content/home.html'
     context = {
         'title': title,
+    }
+    return render(request, template, context)
+
+
+def not_found(request, unknown_page='unknown_page'):
+
+    """ Render the 404 not found template """
+
+    template = 'content/404.html'
+    context = {
+        'unknown_page': unknown_page,
     }
     return render(request, template, context)
